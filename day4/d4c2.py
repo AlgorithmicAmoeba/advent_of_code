8998a48c59c85a91504587221dbca3ed455b6357
Darren Roos <darren.roos@proquoai.com>
2021-12-04 07:35:41 +0200

    Refactor

--- day4/d4c2.py
+++ day4/d4c2.py
@@ -1,42 +1,5 @@
 import numpy
-
-
-class Grid:
-    def __init__(self, g):
-        self.g = g
-        self.b = numpy.full_like(g, fill_value=1)
-
-    def mark(self, number):
-        self.b -= (self.g == number)
-
-    def is_winning(self):
-        return any(numpy.sum(self.b, axis=0) == 0) or any(numpy.sum(self.b, axis=1) == 0)
-
-    def score(self):
-        return numpy.sum(self.g * self.b)
-
-
-def read_input(filename):
-    with open(filename, 'r') as f:
-        numbers = [int(i) for i in next(f).strip().split(',')]
-        next(f)
-        grids = []
-        A = []
-        for l in f:
-            next_line = l.strip()
-            if next_line == '':
-                grids.append(Grid(numpy.array(A)))
-                A = []
-                continue
-
-            nums = numpy.array([
-                int(n) for n in next_line.split()
-            ])
-            A.append(nums)
-        grids.append(Grid(numpy.array(A)))
-
-        return numbers, grids
-
+from day4.d4c1 import read_input
 
 
 if __name__ == "__main__":
