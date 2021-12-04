8887976a50df76a264a9d24d5bc2af0e8cb23707
Darren Roos <darren.roos@proquoai.com>
2021-12-04 16:10:24 +0200

    Add template

---
+++ template/c1.py
@@ -0,0 +1,16 @@
+
+
+
+def read_input(filename):
+    with open(filename, 'r') as f:
+        for l in f:
+            yield l
+
+
+
+if __name__ == "__main__":
+    do_example = False
+    input_file = "example.txt" if do_example else "input.txt"
+
+    for l in read_input(input_file):
+        print(l)
