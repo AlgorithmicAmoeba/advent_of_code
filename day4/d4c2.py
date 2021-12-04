3c699ad914c11a43d7efd53c680ede1362ab8862
Darren Roos <darren.roos@proquoai.com>
2021-12-04 07:24:12 +0200

    Day 4

---
+++ day4/d4c2.py
@@ -0,0 +1,61 @@
+import numpy
+
+
+class Grid:
+    def __init__(self, g):
+        self.g = g
+        self.b = numpy.full_like(g, fill_value=1)
+
+    def mark(self, number):
+        self.b -= (self.g == number)
+
+    def is_winning(self):
+        return any(numpy.sum(self.b, axis=0) == 0) or any(numpy.sum(self.b, axis=1) == 0)
+
+    def score(self):
+        return numpy.sum(self.g * self.b)
+
+
+def read_input(filename):
+    with open(filename, 'r') as f:
+        numbers = [int(i) for i in next(f).strip().split(',')]
+        next(f)
+        grids = []
+        A = []
+        for l in f:
+            next_line = l.strip()
+            if next_line == '':
+                grids.append(Grid(numpy.array(A)))
+                A = []
+                continue
+
+            nums = numpy.array([
+                int(n) for n in next_line.split()
+            ])
+            A.append(nums)
+        grids.append(Grid(numpy.array(A)))
+
+        return numbers, grids
+
+
+
+if __name__ == "__main__":
+    do_example = False
+    input_file = "example.txt" if do_example else "input.txt"
+
+    ns, gs = read_input(input_file)
+
+    lasts = numpy.full(len(gs), fill_value=1)
+    done = False
+    for n in ns:
+        if done:
+            break
+        for i, g in enumerate(gs):
+            g.mark(n)
+            if g.is_winning():
+                lasts[i] = 0
+
+            if all(lasts == 0):
+                print(g.score() * n)
+                done = True
+                break
